02a3a3cc6d56b11240929db5670190d69b8d6e7a
kae0408 <k.tazawa.k@gmail.com>
2018-08-12 03:40:56 +0000

    ユーザー編集画像update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get "users/signup" => "users#new"
+  
+  post "users/:id/update" => "users#update"
+  get "users/:id/edit" => "users#edit"
+  post "users/create" => "users#create"
+  get "signup" => "users#new"
   get "users/index" => "users#index"
   get "users/:id" => "users#show"
   
