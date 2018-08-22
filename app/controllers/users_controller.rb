da99eb1612e08f7197a45698e0b109bae6f2618d
kae0408 <k.tazawa.k@gmail.com>
2018-08-22 21:55:46 +0000

    ユーザーパスワード設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,10 +15,12 @@ class UsersController < ApplicationController
     @user = User.new(
       name: params[:name],
       email: params[:email],
+      password: params[:password],
       # 引数を追加し、初期画像が設定されるようにしてください
       image_name: "default_user.jpg"
     )
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "ユーザー登録が完了しました"
       redirect_to("/users/#{@user.id}")
     else
@@ -56,9 +58,31 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
   
-  def login_form
+  def login
+    # 入力内容と一致するユーザーを取得し、変数@userに代入してください
     
+    @user = User.find_by(email:  params[:email], password: params[:password]) 
+    # @userが存在するかどうかを判定するif文を作成してください
+    if @user
+      session[:user_id] = @user.id
+      
+      flash[:notice] = "ログインしました"
+      redirect_to("/posts/index")
+    else
+      @error_message = "メールアドレスまたはパスワードが違います"
+    
+      @email = params[:email]
+      @password = params[:password]
+      render("users/login_form")
+    end
   end
 
+  
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました"
+    redirect_to("/login")
+  end
+  
   
 end
